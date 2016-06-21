88eaf5eb5a117182b0cd94ce8ae605f15620f2db
Khanh Bao Ha Trinh <khanh.tbh@gmail.com>
2016-06-21 09:55:29 +0700

    Initial Commit

---
+++ BetterKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BetterKVO
+//
+//  Created by Khanh Bao Ha Trinh on 6/21/16.
+//  Copyright © 2016 Kei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BetterKVO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BetterKVO
+//
+//  Created by Khanh Bao Ha Trinh on 6/21/16.
+//  Copyright © 2016 Kei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
